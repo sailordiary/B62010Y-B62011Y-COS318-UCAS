e88859c72979add89ed3aec48cc926b163594b1e
Yuan-Hang Zhang <sailordiary@users.noreply.github.com>
2018-01-21 16:09:26 +0800

    File name correction

--- lab/project6-start-code/run.sh
+++ lab/project6-start-code/run.sh
@@ -3,6 +3,6 @@ sudo umount mnt
 rm image
 dd if=/dev/zero of=image bs=4096 count=1 conv=notrunc
 make clean && make
-./p6fs disk.img mnt -o logfile="log"
+./p6fs image mnt -o logfile="log"
 sleep 1
 cat log
